1d202a0bd52388b5f78ed6d965b57ab2e049692b
Muhammad Junaid <engr.muhammadjunaid@hotmail.com>
2022-07-28 17:28:52 +0500

    fix issues of about and contact page

--- app.js
+++ app.js
@@ -47,11 +47,11 @@ app.get("/", function (req, res) {
 });
 
 app.get("/about", function (req, res) {
-	res.render("about", { aboutContent: aboutContent });
+	res.render("about", { content: aboutContent });
 });
 
 app.get("/contact", function (req, res) {
-	res.render("contact", { contactContent: contactContent });
+	res.render("contact", { content: contactContent });
 });
 
 app.get("/compose", function (req, res) {
