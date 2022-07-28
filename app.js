61758b6bd0f6e1e4da6387e653351f8e43a54ec8
iamjunaidjutt <engr.muhammadjunaid@hotmail.com>
2022-07-28 17:12:03 +0500

    connect mongoDB Atlas with the website

--- app.js
+++ app.js
@@ -8,13 +8,16 @@ const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui 
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
-mongoose.connect("mongodb://localhost:27017/blogDB", (err, db) => {
-	if (err) {
-		console.log(err.message);
-	} else {
-		console.log("Connected to MongoDB Database");
-	}
-});
+const urlDB = "mongodb+srv://admin-junaid:KZ0yj8yXxmmY81bi@cluster0.afaucgl.mongodb.net/blogDB?retryWrites=true&w=majority";
+
+mongoose.connect(urlDB, {
+	useNewUrlParser: true, 
+	useUnifiedTopology: true 
+ }).then(() => {
+	console.log("Connect to database");
+ }).catch(err => {
+	console.log(err.message);
+ });
 
 const postSchema = mongoose.Schema({
 	title: String,
@@ -82,6 +85,11 @@ app.get("/posts/:postId", function (req, res) {
 	});
 });
 
-app.listen(3000, function () {
-	console.log("Server started on port 3000");
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port, function () {
+	console.log("Server started Successfully");
 });
